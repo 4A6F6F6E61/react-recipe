b1c81d18f94e9e3bc04d059ac378ee330a64dca0
Joona Brückner <joona.brueckner@gmail.com>
2025-11-05 00:49:37 +0100

    Add Native Bottom Navigation Tab Bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,117 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
+import { isLiquidGlassAvailable } from "expo-glass-effect"
+import {
+  Icon,
+  Label,
+  NativeTabs,
+  VectorIcon,
+} from "expo-router/unstable-native-tabs"
+import React from "react"
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useThemeColor } from "@/hooks/use-theme-color"
+import { theme } from "@/theme"
+import {
+  ColorValue,
+  DynamicColorIOS,
+  ImageSourcePropType,
+  Platform,
+} from "react-native"
+
+type VectorIconFamily = {
+  getImageSource: (
+    name: string,
+    size: number,
+    color: ColorValue,
+  ) => Promise<ImageSourcePropType>
+}
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const tintColor = useThemeColor(theme.color.reactBlue, "tint")
+  const inactiveTintColor = useThemeColor(
+    {
+      light: "#00000090",
+      dark: "#FFFFFF90",
+    },
+    "tint",
+  )
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    <NativeTabs
+      badgeBackgroundColor={tintColor}
+      labelStyle={{
+        color:
+          Platform.OS === "ios" && isLiquidGlassAvailable()
+            ? DynamicColorIOS({
+                light: theme.colorBlack,
+                dark: theme.colorWhite,
+              })
+            : inactiveTintColor,
+      }}
+      iconColor={
+        Platform.OS === "ios" && isLiquidGlassAvailable()
+          ? DynamicColorIOS({
+              light: theme.colorBlack,
+              dark: theme.colorWhite,
+            })
+          : inactiveTintColor
+      }
+      tintColor={
+        Platform.OS === "ios"
+          ? DynamicColorIOS(theme.color.reactBlue)
+          : inactiveTintColor
+      }
+    >
+      <NativeTabs.Trigger name="recipes">
+        <Label>Recipes</Label>
+        {Platform.select({
+          ios: <Icon sf="book.fill" />,
+          android: (
+            <Icon
+              src={
+                <VectorIcon
+                  family={MaterialCommunityIcons as VectorIconFamily}
+                  name="book"
+                />
+              }
+              selectedColor={tintColor}
+            />
+          ),
+        })}
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="ingredients">
+        <Label>Ingredients</Label>
+        {Platform.select({
+          ios: <Icon sf="basket.fill" />,
+          android: (
+            <Icon
+              src={
+                <VectorIcon
+                  family={MaterialCommunityIcons as VectorIconFamily}
+                  name="basket"
+                />
+              }
+              selectedColor={tintColor}
+            />
+          ),
+        })}
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="settings">
+        <Label>Settings</Label>
+        {Platform.select({
+          ios: <Icon sf="gear" />,
+          android: (
+            <Icon
+              src={
+                <VectorIcon
+                  family={MaterialCommunityIcons as VectorIconFamily}
+                  name="cog"
+                />
+              }
+              selectedColor={tintColor}
+            />
+          ),
+        })}
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  )
 }
